44eb2a4b92415d0a763e29383d63bb4575127c91
Lasse Kolb <34098611+lassekolb@users.noreply.github.com>
2025-03-18 20:18:04 +0100

    day 5 sol 1

--- 5/Solution.scala
+++ 5/Solution.scala
@@ -8,30 +8,34 @@ object Main extends App {
 
   val emptyLineIndex = allLines.indexWhere(_.trim.isEmpty)
 
-  // Split the list into two parts at the empty line index
   val (firstPart, secondPart) = allLines.splitAt(emptyLineIndex)
 
-  // Remove the empty line from the second part
   val cleanedSecondPart = secondPart.drop(1)
 
-  // Convert the lines in each part into lists of integers
-  val list1 = firstPart.map(_.split("\\|").map(_.toInt).toList)
-  val list2 = cleanedSecondPart.map(_.split(",").map(_.toInt).toList)
+  val rules = firstPart.map(_.split("\\|").map(_.toInt).toList)
+  val updates = cleanedSecondPart.map(_.split(",").map(_.toInt).toList)
 
-  // Example printing to verify the output
-  println("List 1:")
-  list1.foreach(println)
-
-  println("List 2:")
-  list2.foreach(println)
+  val result1 = Solution.solution1(rules, updates)
+  println(result1)
 }
 
 object Solution {
-  def solution1(rules: List[Int], updates: List[Int]): Int = {
-    for {
-      update <- updates
-      rule <- update
+  def checkRule(rule: List[Int], update: List[Int]): Boolean = {
+    !(update.contains(rule(0)) && update.contains(rule(1))) || update.indexOf(
+      rule(0)
+    ) < update.indexOf(rule(1))
+  }
 
+  def checkRules(rules: List[List[Int]], update: List[Int]): Int = {
+    if (rules.forall(rule => checkRule(rule, update))) {
+      update(update.length / 2)
+    } else {
+      0
+    }
+  }
+  def solution1(rules: List[List[Int]], updates: List[List[Int]]): Int = {
+    updates.foldLeft(0) { (acc, update) =>
+      acc + checkRules(rules, update)
     }
   }
 }
